4a9fea141b0abb7edc2900efceb5b5884578dedb
Laura <laura.laukiz@gmail.com>
2025-04-29 14:01:40 +0200

    endpoints

--- src/index.js
+++ src/index.js
@@ -16,7 +16,6 @@ const server = express();
 //3. Configurar el servidor (permitir que el servidor ecepte peticiones)
 server.use(cors());
 server.use(express.json({limit: "25mb"})); //para limitar el tamaño
-server.set("view engine, ejs"); //para usar EJS
 
 require("dotenv").config(); 
 
@@ -25,6 +24,127 @@ const serverPort = 4001;
 server.listen(serverPort, ()=>{
     console.log("server is running on http://localhost:" + serverPort);
 });
+
+//5. Función para conexión con la DB
+async function getDBConnection(){
+    const connection = await mysql.createConnection({
+        host: "127.0.0.1",
+        user: "root",
+        password: "cucocuco",
+        database:  "strongtrack_db"
+    })
+    connection.connect();
+    return connection;
+}
+
+//Probar conexion DB
+/* server.get('/test-db', async (req, res) => {
+    try {
+      const conn = await getDBConnection();
+      await conn.query('SELECT 1'); // consulta dummy para testear conexión
+      conn.end();
+      res.json({ success: true, message: 'Conexión a la base de datos OK' });
+    } catch (err) {
+      res.status(500).json({ success: false, message: 'Error de conexión a la base de datos', error: err.message });
+    }
+  }); */
+
+//6. ENDPOINTS
+server.get("/strongtrack/entrenamientos", async (req, res) => {
+   const connection = await getDBConnection();
+   const sqlQuery = "SELECT * FROM entrenamientos";
+   const [entrenamientosResult] = await connection.query(sqlQuery);
+    connection.end();
+
+   res.status(200).json({
+    info: {
+        count: entrenamientosResult.length
+    },
+    result: entrenamientosResult
+    
+   });
+
+})
+
+server.get("/strongtrack/entrenamiento/:id", async (req, res) => {
+
+    const connection = await getDBConnection();
+    const {id} = req.params;
+    const sqlQuery = "SELECT * FROM entrenamientos WHERE id = ?";
+    const [entrenamientoResult] = await connection.query(sqlQuery, [id]);
+    connection.end();
+
+    res.status(200).json({
+        status: "seccess",
+        result: entrenamientoResult
+    });
+
+}) 
+
+server.post("/strongtrack/entrenamiento", async (req, res) => {
+  const connection = await getDBConnection();
+  const { fecha, grupo_muscular, equipamiento, descripcion, series, repeticiones, tiempo_descanso, fk_usuario } = req.body;
+
+  if (!fecha || !grupo_muscular || !equipamiento || !descripcion || !series || !repeticiones || !tiempo_descanso || !fk_usuario) {
+    return res.status(400).json({
+      success: false,
+      message: "Bad params. Provide all params"
+    });
+  }
+
+  const sqlQuery = "INSERT INTO entrenamientos (fecha, grupo_muscular, equipamiento, descripcion, series, repeticiones, tiempo_descanso, fk_usuario) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+  const [entrenamientoResult] = await connection.query(sqlQuery, [
+    fecha,
+    grupo_muscular,
+    equipamiento,
+    descripcion,
+    series,
+    repeticiones,
+    tiempo_descanso,
+    fk_usuario
+  ]);
+  connection.end();
+
+  res.status(201).json({
+    success: true,
+    id: entrenamientoResult.insertId
+  });
+});
+
+server.put("/strongtrack/entrenamiento/:id", async (req, res) => {
+
+    const connection = await getDBConnection();
+    const {id} = req.params;
+    const {fecha, grupo_muscular, equipamiento, descripcion, series, repeticiones, tiempo_descanso, fk_usuario} = req.body; 
+    const sqlQuery = "UPDATE entrenamientos SET fecha = ?, grupo_muscular = ?, equipamiento = ?, descripcion = ?, series = ?, repeticiones = ?, tiempo_descanso = ? WHERE id = ?";
+    const [entrenamientoResult] = await connection.query(sqlQuery, [fecha, grupo_muscular, equipamiento, descripcion, series, repeticiones, tiempo_descanso, id]);
+   
+    connection.end();
+
+    res.status(200).json({
+        success: true,
+        id: entrenamientoResult.insertId
+    });
+
+})
+
+server.delete("/strongtrack/entrenamiento/:id", async (req, res) => {
+
+    const connection = await getDBConnection();
+    const {id} = req.params;
+    const sqlQuery = "DELETE FROM entrenamientos WHERE id = ?";
+    const [entrenamientoResult] = await connection.query(sqlQuery, [id]);
+
+    connection.end();
+
+    res.status(200).json({
+        status: "success",
+        message: "Removed resource"
+    });
+
+})
+
+
 
 //Servidor de estaticos
 const staticServerPath = "./web"; //carpeta de mis ficheros html, ruta de la raiz del proyecto
